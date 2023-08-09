d7522a7585b88be70963c10b78746faba5f60d76
shengweiDing <dingshengwei1999@163.com>
2023-08-09 12:02:04 +0800

    第一次提交

---
+++ src/main/java/com/papa/bittergourd/BitterGourdApplication.java
@@ -0,0 +1,13 @@
+package com.papa.bittergourd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitterGourdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitterGourdApplication.class, args);
+	}
+
+}
