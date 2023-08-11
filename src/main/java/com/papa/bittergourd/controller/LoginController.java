682f130002a1cb5c3a918eae03c94b9afa703733
shengweiDing <dingshengwei1999@163.com>
2023-08-11 18:14:54 +0800

    提交测试

--- src/main/java/com/papa/bittergourd/controller/LoginController.java
+++ src/main/java/com/papa/bittergourd/controller/LoginController.java
@@ -4,6 +4,9 @@ package com.papa.bittergourd.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ *  登陆接口
+ */
 @RestController
 @RequestMapping("login")
 public class LoginController {
